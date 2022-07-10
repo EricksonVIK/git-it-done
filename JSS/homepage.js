201c625aff5af54b1b6b1a5e6a94d22031a7d39b
Erikson <chris.s.erickson@gmail.com>
2022-07-10 14:06:58 -0400

    added parameter 'user' to fetch

--- JSS/homepage.js
+++ JSS/homepage.js
@@ -1,7 +1,26 @@
-var getUserRepos = function(){
-    // console.log("function was called") -- console log used to verify function was being called
-    // used curl and ping in terminal to verify connection with api
-    fetch("https://api.github.com/users/octocat/repos");
+// var getUserRepos = function(){
+//     // console.log("function was called") -- console log used to verify function was being called
+//     // used curl and ping in terminal to verify connection with api
+//     var response =fetch("https://api.github.com/users/octocat/repos").then(function(response){
+//         response.json().then(function (data){
+//             console.log(data);
+//         });
+//         console.log("inside", response)
+//     });
+//     console.log("outside");
+// };
+// ABOVE was used t verify access to data
+
+var getUserRepos = function(user){
+    // format the github api url
+    var apiUrl = "http://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+        console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+getUserRepos("EricksonVIK");
